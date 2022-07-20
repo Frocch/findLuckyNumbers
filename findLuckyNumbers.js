a6f463de49557b9f678d1bbdb64099ee05d54e50
Federico Rocchetti <federico_rocchetti@hotmail.com>
2022-07-19 21:43:22 -0400

    Problem Solved

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,10 @@
 // Write your code below this line.
+
+const luckyNumbers = n => {
+    let numsArr = []
+    for (let i = 0; i < n; i++) {
+        numsArr.push(Math.floor(Math.random() * 10) + 1)
+    } return numsArr
+}
+
+console.log(luckyNumbers(5))
